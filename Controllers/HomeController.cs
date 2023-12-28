01c3d91500a1154884ad736ef6c7022348f9dbbc
Rachana Patel <rachanapatel01.vision@gmail.com>
2023-12-28 09:15:00 +0530

    code optimization

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -19,12 +19,12 @@ namespace RealTimeDataReflection.Controllers
         public IActionResult Index()
         {
             return View();
-        }     
-        
+        }  
         public List<Student> GetAllStudents()
         {
             string connectionString = _configuration.GetConnectionString("ConnectionString");
-            var studentList = new List<Student>(); try
+            var studentList = new List<Student>(); 
+            try
             {
                 using (SqlConnection con = new SqlConnection(connectionString))
                 {
@@ -61,6 +61,27 @@ namespace RealTimeDataReflection.Controllers
         private void DbChangeNotification(object sender, SqlNotificationEventArgs e)
         {
             _context.Clients.All.SendAsync("RefreshStudentData");
+        }
+
+        public bool DeleteRecord(int rollno)
+        {
+            string connectionString = _configuration.GetConnectionString("ConnectionString");
+            try
+            {
+                using(SqlConnection con = new SqlConnection(connectionString))
+                {
+                    con.Open();
+                    string query = "Delete from Student where rollno=@rollno";
+                    SqlCommand cmd = new SqlCommand(query,con);
+                    cmd.Parameters.AddWithValue("@rollno",rollno);
+                    cmd.ExecuteNonQuery();
+                    return true;
+                }
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
         }
     }
 }
--- wwwroot/js/StudentLiveDataScript.js
+++ wwwroot/js/StudentLiveDataScript.js
@@ -3,8 +3,10 @@
     let connection = new signalR.HubConnectionBuilder().withUrl("/signalServer").build();
     connection.start();
     connection.on("RefreshStudentData", function () {
-        GetAllStudents();
+        //GetAllStudents();
+        console.log('connected....')
     })
+
     GetAllStudents();
 
     function GetAllStudents() {
@@ -27,6 +29,7 @@
                             "<td>" + studentList[i].lname + "</td>" +
                             "<td>" + studentList[i].city + "</td>" +
                             "<td>" + studentList[i].mobilenum + "</td>" +
+                            "<td>" + "<input type='button' value='Delete' class='btn btn-default btn-danger' onclick=\"DeleteRecord('" + studentList[i].rollno + "')\">" + "</td>" +
                             "</tr>";
 
                         setStudentList.insertAdjacentHTML('beforeend', data);
@@ -42,9 +45,28 @@
 
         xhr.send();
     }
+
+    setInterval(GetAllStudents, 5000);
 }) 
 
-
+function DeleteRecord(rollno) {
+    if (confirm('Are you Sure you want to Delete Record..??')) {
+        var xhr = new XMLHttpRequest();
+        xhr.open("DELETE", "/Home/DeleteRecord?rollno=" + rollno, true);
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState == XMLHttpRequest.DONE) {
+                if (xhr.status == 200) {
+                    var employee = xhr.responseText;
+                    console.log(employee);
+                    alert('Record Deleted Successfully..!!');
+                    //document.getElementById('employeeList').innerHTML = '';
+                    //GetAllRecords();
+                }
+            }
+        }
+        xhr.send();
+    }
+}
 
 
 
